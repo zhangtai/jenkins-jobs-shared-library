791820ac281776ffc7abd080ca63cdc28c43fbe1
Tai Zhang <tai.zhang@outlook.com>
2021-04-25 16:08:10 +0800

    Create function createAttribute createControl

--- vars/createModels.groovy
+++ vars/createModels.groovy
@@ -4,6 +4,7 @@ import jenkins.model.Jenkins
 import com.cloudbees.hudson.plugins.modeling.impl.auxiliary.AuxModel
 import com.cloudbees.hudson.plugins.modeling.Attribute
 import com.cloudbees.hudson.plugins.modeling.controls.*
+import com.cloudbees.hudson.plugins.modeling.UIControl
 
 @Field Jenkins jenkins = Jenkins.get()
 
@@ -17,24 +18,8 @@ def createModel(Map modelDef, String superType = null) {
     // Add attributes
     def attributes = []
     modelDef?.attrs?.each { Map attrDef ->
-        println "Creating attribute: ${attrDef}"
-        Map controlDef = attrDef.control as Map
-        def control = null
-        if (controlDef.type == "TextFieldControl") {
-            control = new TextFieldControl()
-        }
-        if (controlDef.type == "ChoiceControl") {
-            def options = controlDef.options.collect { Map option ->
-                new ChoiceControl.Option(option.display as String, option.value as String)
-            }
-            Map modes = [
-                    "DROPDOWN_LIST": ChoiceControl.Mode.DROPDOWN_LIST,
-                    "RADIO_BUTTON": ChoiceControl.Mode.RADIO_BUTTON
-            ]
-            control = new ChoiceControl(options, modes.get(controlDef.mode))
-        }
-        def attr = new Attribute(attrDef.name as String, attrDef.displayName as String, control)
-        attributes << attr
+        Attribute attribute = createAttribute(attrDef)
+        attributes << attribute
     }
     println "Setting attributes"
     auxModel.setAttributes(attributes)
@@ -48,6 +33,32 @@ def createModel(Map modelDef, String superType = null) {
             createModel(childModelDef as Map, modelDef.name as String)
         }
     }
+}
+
+Attribute createAttribute(Map attrDef) {
+    println "Creating attribute: ${attrDef}"
+    Map controlDef = attrDef.control as Map
+    UIControl control = createControl(controlDef)
+    def attr = new Attribute(attrDef.name as String, attrDef.displayName as String, control)
+    return attr
+}
+
+static UIControl createControl(Map controlDef) {
+    UIControl control = null
+    if (controlDef.type == "TextFieldControl") {
+        control = new TextFieldControl()
+    }
+    if (controlDef.type == "ChoiceControl") {
+        def options = controlDef.options.collect { Map option ->
+            new ChoiceControl.Option(option.display as String, option.value as String)
+        }
+        Map modes = [
+                "DROPDOWN_LIST": ChoiceControl.Mode.DROPDOWN_LIST,
+                "RADIO_BUTTON": ChoiceControl.Mode.RADIO_BUTTON
+        ]
+        control = new ChoiceControl(options, modes.get(controlDef.mode))
+    }
+    return control
 }
 
 def createAllModels() {
